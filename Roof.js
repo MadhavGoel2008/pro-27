b3d7c404b0f106650911593ef9c6f8e647529922
MadhavGoel2008 <79358645+MadhavGoel2008@users.noreply.github.com>
2021-04-10 09:56:23 +0530

    Add files via upload

---
+++ Roof.js
@@ -0,0 +1,18 @@
+class Ground {
+    constructor(x,y,width,height) {
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    }
+    display(){
+     
+     Push();
+     rectMode(CENTER); 
+     fill(128, 128, 128);
+     
+     rect(this.body.position.x,this.body.position.y, this.width, this.height);
+    Pop();
+    
+  }
+  };
